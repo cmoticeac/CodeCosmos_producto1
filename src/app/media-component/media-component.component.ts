5f7f406bf4e91ec5cdc55e29c03769e1fe3074ee
cmoticeac <147082946+cmoticeac@users.noreply.github.com>
2024-10-09 21:03:04 +0200

    Componentes

---
+++ src/app/media-component/media-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-media-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './media-component.component.html',
+  styleUrl: './media-component.component.css'
+})
+export class MediaComponentComponent {
+
+}
