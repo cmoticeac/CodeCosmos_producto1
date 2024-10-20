6c94fd44cd71daf112d551e08a49c0d7c7fd9f75
cmoticeac <147082946+cmoticeac@users.noreply.github.com>
2024-10-20 08:15:41 +0200

    acutalizacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+
 import { PlayersComponent } from "./players-component/players-component.component";
 import { DetailComponent } from "./detail-component/detail-component.component";
 import { MediaComponent } from "./media-component/media-component.component";
+import { InicioComponent } from "./inicio/inicio.component";
+import { ListaEquiposComponent } from './lista-equipos/lista-equipos.component';
+import { CommonModule } from '@angular/common';
 
+import listadoNombreEquipo from '../assets/datos-equipos.json';
+ 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PlayersComponent, DetailComponent, MediaComponent],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, PlayersComponent, DetailComponent, MediaComponent, InicioComponent, ListaEquiposComponent,CommonModule],
+  templateUrl:'./app.component.html',
   styleUrl: './app.component.css'
-})
-export class AppComponent {
-  title = 'producto1_CodeCosmos';
+}) 
+export class AppComponent implements OnInit  {
+  
+  // aqui se carga componente de ver equipos
+  componenteVisible:boolean = false
+  nombreEquipo:any[] =[]
+  
+  muestraEquipos() {  
+    this.componenteVisible = true;
+    //alert('visible: ' + this.componenteVisible)
+  }
+
+  /*
+      Al pulsar el select de equipo ( el objetivo seria ver los componentes)
+  */
+  onSelect(equipo: string) {
+    //console.log('Equipo seleccionado:', equipo);
+    alert(equipo)
+    // Aquí puedes agregar la lógica que quieres ejecutar cuando se selecciona un equipo
+  }
+
+  ngOnInit(): void {
+    this.nombreEquipo =listadoNombreEquipo;
+    // alert(listadoNombreEquipo)
+    this.componenteVisible = false;  
+  }
 }
+
