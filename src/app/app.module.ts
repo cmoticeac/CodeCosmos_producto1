7c3823c04a9147cce2b1c45e3e8018b1616c407c
cmoticeac <147082946+cmoticeac@users.noreply.github.com>
2024-10-21 14:57:54 +0200

    Revert "."
    This reverts commit 3ec9809a5a232101b4aa74a426102d994a7e1324.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
+
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthInterceptorService } from './service/auth-interceptor.service';
 import { AppComponent } from './app.component';
+import { ListaEquiposComponent } from './lista-equipos/lista-equipos.component';
+import { ServicioCliente } from './servicio-cliente/servicio-cliente.component';
 
 @NgModule({
   imports: [
+    BrowserModule
     BrowserModule,
     HttpClientModule,
   ],
   providers: [
+    ServicioCliente,
     provideHttpClient(),
     importProvidersFrom(AppComponent), // Importa el AppComponent como standalone
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true }
